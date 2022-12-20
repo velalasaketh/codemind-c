61d30e653f38f01851ee26b21bb5ea0c3b2b3831
velalasaketh <119733812+velalasaketh@users.noreply.github.com>
2022-12-20 20:33:14 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int i,gcd;     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0&&b%i==0)         {             gcd=i;         }     }     printf("%d",gcd); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int i,gcd;
+    for(i=1;i<=a&&i<=b;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    printf("%d",gcd);
+}
